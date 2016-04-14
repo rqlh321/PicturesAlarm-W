1eb0e605f9fd69236bc186bbf4a454b2b06a7eac
rqlh321 <rqlh321@gmail.com>
2016-04-14 19:46:35 +0300

    1) выбор источника данных - папка во внутренней памяти или SD-карте

--- app/src/main/java/com/example/sic/slideshow/MainActivity.java
+++ app/src/main/java/com/example/sic/slideshow/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.sic.slideshow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView setDirText;
+    Button setDirButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setDirText= (TextView) findViewById(R.id.setDirTextView);
+        setDirButton= (Button) findViewById(R.id.setDirButton);
+
+        setDirButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,OpenDir.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (data == null) {return;}
+        String url = data.getStringExtra("url");
+        setDirText.setText(url);
     }
 }
