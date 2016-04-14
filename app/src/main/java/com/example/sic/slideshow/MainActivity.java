055b890d96493fd6ee18d55c2ef01e9ca8dbd5a2
rqlh321 <rqlh321@gmail.com>
2016-04-15 02:17:29 +0300

    UI

--- app/src/main/java/com/example/sic/slideshow/MainActivity.java
+++ app/src/main/java/com/example/sic/slideshow/MainActivity.java
@@ -5,26 +5,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.Formatter;
 
 public class MainActivity extends AppCompatActivity {
-    TextView setDirText;
+    EditText setDirText;
     Button setDirButton;
+    Button startService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setDirText= (TextView) findViewById(R.id.setDirTextView);
+        setDirText= (EditText) findViewById(R.id.setDirTextView);
         setDirButton= (Button) findViewById(R.id.setDirButton);
+        startService= (Button) findViewById(R.id.startService);
+
+        final TimePicker tpStart= (TimePicker) findViewById(R.id.startTime);
+        final TimePicker tpEnd= (TimePicker) findViewById(R.id.endTime);
+        tpStart.setIs24HourView(true);
+        tpStart.setCurrentHour((new Date()).getHours());
+        tpEnd.setIs24HourView(true);
+        tpEnd.setCurrentHour((new Date()).getHours());
+
+        NumberPicker numberPicker= (NumberPicker) findViewById(R.id.intervalNumber);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(60);
+        numberPicker.setValue(1);
 
         setDirButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,OpenDir.class);
                 startActivityForResult(intent, 1);
+            }
+        });
+
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!setDirText.getText().toString().isEmpty()){
+                    Date start=new Date();
+                    start.setHours(tpStart.getCurrentHour());
+                    start.setMinutes(tpStart.getCurrentMinute());
+                    Date end=new Date();
+                    end.setHours(tpEnd.getCurrentHour());
+                    end.setMinutes(tpEnd.getCurrentMinute());
+                    Date currentDate=new Date();
+                    if(start.after(currentDate)){
+                        if(end.after(start)){
+                            Toast.makeText(MainActivity.this, R.string.start_service, Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,R.string.wrong_end, Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this,R.string.wrong_start, Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, R.string.wrong_folder, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
