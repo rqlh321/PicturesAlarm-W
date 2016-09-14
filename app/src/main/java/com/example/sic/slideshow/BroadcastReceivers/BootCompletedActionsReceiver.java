f7be4fa2fae2e41c4785144ab14fdf3340e429bf
rqlh321 <rqlh321@gmail.com>
2016-09-14 14:58:23 +0300

    NotificationManager refactoring

--- app/src/main/java/com/example/sic/slideshow/BroadcastReceivers/BootCompletedActionsReceiver.java
+++ app/src/main/java/com/example/sic/slideshow/BroadcastReceivers/BootCompletedActionsReceiver.java
@@ -3,8 +3,11 @@ package com.example.sic.slideshow.BroadcastReceivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
+import com.example.sic.slideshow.Activity.MainActivity;
 import com.example.sic.slideshow.Activity.ViewActivity;
+import com.example.sic.slideshow.MyService;
 
 /**
  * Created by sic on 06.09.2016.
@@ -12,8 +15,14 @@ import com.example.sic.slideshow.Activity.ViewActivity;
 public class BootCompletedActionsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent intentForView = new Intent(context, ViewActivity.class);
-        intentForView.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intentForView);
+        SharedPreferences mSettings = context.getSharedPreferences(MainActivity.APP_PREFERENCES, Context.MODE_PRIVATE);
+        String id = mSettings.getString(MainActivity.APP_PREFERENCES_ID, "");
+        if (!id.equals("")) {
+            Intent service = new Intent(context, MyService.class);
+            context.startService(service);
+            Intent intentForView = new Intent(context, ViewActivity.class);
+            intentForView.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intentForView);
+        }
     }
 }
